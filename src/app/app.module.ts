7dfdecfd586911c8f8a14a34ec4e0da6ee4c8d4f
karthick2773 <karthick.ramanathan96@gmail.com>
2022-10-31 21:26:33 +0530

    new folder creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Headers } from './headers/headers.component';
+import { RecipiesComponent } from './recipes/recipies.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListDetailComponent } from './shopping-list/shopping-list-detail/shopping-list-detail.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Headers,
+    RecipiesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/headers/headers.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./headers.component.html'
+})
+export class Headers{
+
+}
